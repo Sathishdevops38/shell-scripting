b6d96838b506fc4409ac23f5cde3d282d2db6b84
sathishdevops38 <sathishdevops38@gmail.com>
2025-09-19 16:46:44 +0530

    last command

--- 06-default-var.sh
+++ 06-default-var.sh
@@ -5,4 +5,4 @@ echo "current directroy $PWD"
 echo "user name is $USER"
 echo "currect use home dic $HOME"
 echo "current script PID is $$"
-echo "last run command is $!"
+echo "last run command is" !!
