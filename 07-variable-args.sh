7861aee00467b8d516f41fd6860a4e9ee074ae2a
sathishdevops38 <sathishdevops38@gmail.com>
2025-09-19 16:39:29 +0530

    args commit

---
+++ 07-variable-args.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#without variable 
+# echo "Trump:: Hello Modi"
+# echo "Modi:: Hi Trump How are you?"
+# echo "Trump:: fine how about u?"
+# echo "Modi:: fine"
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hello $PERSON2."
+echo "$PERSON2:: Hi $PERSON1 How are you?"
+echo "$PERSON1:: fine how about u?"
+echo "$PERSON2:: fine."
