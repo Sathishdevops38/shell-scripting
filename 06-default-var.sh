db7c210a6204651d7b352685827ec36a3f1eda94
sathishdevops38 <sathishdevops38@gmail.com>
2025-09-19 16:43:14 +0530

    default var

--- 06-default-var.sh
+++ 06-default-var.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+echo "Special variables $@, $*"
+echo "script name $0"
+echo "current directroy $PWD"
+echo "user name is $USER"
+echo "currect use home dic $HOME"
+echo "current script PID is $$"
+echo "last run command is $!"
