8812de0482964764473d2dcaddadddad9d76cb56
sathishdevops38 <sathishdevops38@gmail.com>
2025-09-19 16:59:45 +0530

    syntax error fix

--- 08-arrays-datatyps.sh
+++ 08-arrays-datatyps.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-name=("linux","shell","Docker")
+name=("linux" "shell" "Docker")
 #total size of array 3 max index 2
 echo "${name[@]}" #to print all
 echo "${name[1]}"
+my_array=("apple" "banana" "cherry" "date")
+echo "The second element is: ${my_array[1]}"
