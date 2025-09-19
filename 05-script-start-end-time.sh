d99819d124eaadf397cbbd5377c745b9e41d7bff
sathishdevops38 <sathishdevops38@gmail.com>
2025-09-19 16:33:49 +0530

    remove space

--- 05-script-start-end-time.sh
+++ 05-script-start-end-time.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #VAR_NAME=$(command)
-start_time= $(date +%s)
+start_time=$(date +%s)
 sleep 10
-end_time= $(date +%s)
-total_time= $(($end_time-$start_time))
+end_time=$(date +%s)
+total_time=$(($end_time-$start_time))
