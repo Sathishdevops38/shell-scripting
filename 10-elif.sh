b209b0cc45f421c54991c153f60062723d7a349a
sathishdevops38 <sathishdevops38@gmail.com>
2025-09-22 10:46:28 +0530

    syntax fix

--- 10-elif.sh
+++ 10-elif.sh
@@ -3,7 +3,7 @@ NUM=$1
 if [ $NUM -lt 10 ]; then
     echo "number $NUM is less than 10"
 elif [ $NUM -eq 10 ]; then
-    echo "number $NUM is equal to 10
+    echo "number $NUM is equal to 10"
 else
     echo "number $NUM is greater than 10"
 fi
