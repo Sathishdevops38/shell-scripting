c6e7bdbe2e1893cbd366ee4bb77cb17d414bcd79
sathishdevops38 <sathishdevops38@gmail.com>
2025-09-23 11:38:25 +0530

    syntax fix

--- 15.color-install.sh
+++ 15.color-install.sh
@@ -8,16 +8,16 @@ N="\e[0m"
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-    echo "$R ERROR::$N Please run this script with $R root $N privelege"
+    echo -e "$R ERROR::$N Please run this script with $R root $N privelege"
     exit 1 # failure is other than 0
 fi
 
 VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo "$R ERROR::$N Installing $2 is $R failure $N"
+        echo -e "$R ERROR::$N Installing $2 is $R failure $N"
         exit 1
     else
-        echo "Installing $2 is $G SUCCESS $N"
+        echo -e "Installing $2 is $G SUCCESS $N"
     fi
 }
 
