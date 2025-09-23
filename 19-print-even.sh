8ea64cb518b98d80e743765df0bdf414034dfe49
sathishdevops38 <sathishdevops38@gmail.com>
2025-09-23 14:33:30 +0530

    print even

---
+++ 19-print-even.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "enter the number ending number up to even num wanna print:"
+read even
+
+for i in $even
+do
+    if [ $($i/2) -eq 0 ]; then
+        echo $i 
+    else 
+        exit 1
+done
