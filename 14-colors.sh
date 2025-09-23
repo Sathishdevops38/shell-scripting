15201adae56577337f51f29af0c675b02cf06da0
sathishdevops38 <sathishdevops38@gmail.com>
2025-09-23 11:25:58 +0530

    colors

---
+++ 14-colors.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo -e "$R Hello World!! $N"
+echo "check this color"
+echo -e "$G Hello World!! $N"
+echo "check this color"
+echo -e "$Y Hello World!! $N"
+echo "check this color"
