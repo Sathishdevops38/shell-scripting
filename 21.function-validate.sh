765a225dfc5f3b0e16e765333a361c75c235389f
sathishdevops38 <sathishdevops38@gmail.com>
2025-09-26 10:57:18 +0530

    syntax

--- 21.function-validate.sh
+++ 21.function-validate.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-add(){
-    read a
-    read b
-    echo "adding two numbers passed by user"
-    add =  $(($a + $b)) 
-    echo "total $a & $b  of two num is: $add"
+adding_num(){
+    num1=$1
+    num2=$2
+    sum=$(($num1 + $num2))
+    echo "the sum of the $num1 and $num2 is : $sum"
 }
 
-add
+echo "enter first_num"
+read first_num
+
+echo "enter second_num"
+read second_num
+
+adding_num "first_num" "second_num"
